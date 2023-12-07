84b43431e60c9a2b919a161492ff87a13242f81c
rizkipohan <rdpohan@gmail.com>
2023-12-07 18:40:45 +0700

    fix step 5

--- controllers/adminOAManualControllers.go
+++ controllers/adminOAManualControllers.go
@@ -40,7 +40,15 @@ func ManualOaRequestCreate(c echo.Context) error {
 		if err != nil {
 			return lib.CustomError(http.StatusInternalServerError, err.Error(), err.Error())
 		}
+	} else if step == "5" {
+		err, oa_request_key := SaveStep5(c)
+		responseData["oa_request_key"] = oa_request_key
+		if err != nil {
+			return lib.CustomError(http.StatusInternalServerError, err.Error(), err.Error())
+		}
 	}
+
+
 
 	var response lib.Response
 	response.Status.Code = http.StatusOK
--- models/adminOAManual.go
+++ models/adminOAManual.go
@@ -427,28 +427,129 @@ func CreateOaBankAccount(paramsBankAccount map[string]string) (error, int64) {
 	return nil, OaRequestKey
 }
 
-type OaRequestBankAccountDetails struct {
-	BankAccountKey  *uint64 `db:"bank_account_key" json:"bank_account_key"`
-	BankKey         *uint64 `db:"bank_key" json:"bank_key"`
-	BankAccountNo   *string `db:"account_no" json:"account_no"`
-	BankAccountName *string `db:"account_holder_name" json:"account_holder_name"`
-	BankBranchName  *string `db:"branch_name" json:"branch_name"`
-	CurrencyKey     *uint64 `db:"currency_key" json:"currency_key"`
-	FlagPriority    *uint64 `db:"flag_priority" json:"flag_priority"`
-}
+func CreateOrUpdateOaRiskProfileQuiz(paramsOaRequest map[string]string, paramsQuizAnswer []OaQuizAnswer) (error, int64) {
+	OaRequestKey, _ := strconv.ParseInt(paramsOaRequest["oa_request_key"], 10, 64)
 
-type CountryModels struct {
-	CountryKey  uint64  `db:"country_key" json:"country_key"`
-	CountryCode string  `db:"country_code" json:"country_code"`
-	CountryName string  `db:"country_name" json:"country_name"`
-	CallingCode *string `db:"calling_code" json:"calling_code"`
-}
+	tx, err := db.Db.Begin()
+	if err != nil {
+		tx.Rollback()
+		log.Println(err.Error())
+		return err, OaRequestKey
+	}
 
-type CityModels struct {
-	CityKey    uint64  `db:"city_key" json:"city_key"`
-	CityCode   string  `db:"city_code" json:"city_code"`
-	CityName   string  `db:"city_name" json:"city_name"`
-	PostalCode *string `db:"postal_code" json:"postal_code"`
+	var total_score int64
+
+	for _, v := range paramsQuizAnswer {
+
+		qGetScore := `SELECT t1.quiz_option_score FROM cms_quiz_options t1 WHERE t1.rec_status = 1 AND t1.quiz_option_key = ` + strconv.FormatInt(v.OptionKey, 10) + ` AND t1.quiz_question_key = ` + strconv.FormatInt(v.QuestionKey, 10)
+
+		var getScore int64
+		err = db.Db.Get(&getScore, qGetScore)
+		if err != nil {
+			tx.Rollback()
+			log.Println(err.Error())
+			return err, OaRequestKey
+		}
+
+		CRUDOaRiskProfileQuiz := make(map[string]string)
+		CRUDOaRiskProfileQuiz["rec_status"] = paramsOaRequest["rec_status"]
+		CRUDOaRiskProfileQuiz["rec_created_by"] = paramsOaRequest["rec_created_by"]
+		CRUDOaRiskProfileQuiz["rec_created_date"] = paramsOaRequest["rec_created_date"]
+		CRUDOaRiskProfileQuiz["oa_request_key"] = strconv.FormatInt(OaRequestKey, 10)
+		CRUDOaRiskProfileQuiz["quiz_question_key"] = strconv.FormatInt(v.QuestionKey, 10)
+		CRUDOaRiskProfileQuiz["quiz_option_key"] = strconv.FormatInt(v.OptionKey, 10)
+		CRUDOaRiskProfileQuiz["quiz_option_score"] = strconv.FormatInt(getScore, 10)
+
+		total_score += getScore
+
+		// CEK ROW SUDAH ADA  ATAU BELUM
+		qCekRows := `SELECT count(*) FROM oa_risk_profile_quiz t1 WHERE t1.rec_status = 1 AND t1.oa_request_key = ` + strconv.FormatInt(OaRequestKey, 10) + ` AND t1.quiz_question_key = ` + strconv.FormatInt(v.QuestionKey, 10)
+		var countRows uint64
+		err := db.Db.Get(&countRows, qCekRows)
+		if err != nil {
+			tx.Rollback()
+			log.Println(err.Error())
+			return err, OaRequestKey
+		}
+
+		qCRUDOaRiskProfileQuiz := ``
+
+		if countRows == 0 { // JIKA TIDAK ADA MAKA CREATE
+			qCRUDOaRiskProfileQuiz = GenerateInsertQuery("oa_risk_profile_quiz", CRUDOaRiskProfileQuiz)
+		} else {
+			qCRUDOaRiskProfileQuiz = "UPDATE oa_risk_profile_quiz SET "
+			i := 0
+			for key, value := range CRUDOaRiskProfileQuiz {
+				if key != "oa_request_key" {
+					if value == "" {
+						qCRUDOaRiskProfileQuiz += key + " = NULL"
+					} else {
+						qCRUDOaRiskProfileQuiz += key + " = '" + value + "'"
+					}
+					if (len(CRUDOaRiskProfileQuiz) - 2) > i {
+						qCRUDOaRiskProfileQuiz += ", "
+					}
+					i++
+				}
+			}
+			qCRUDOaRiskProfileQuiz += " WHERE oa_request_key = " + CRUDOaRiskProfileQuiz["oa_request_key"] + " AND quiz_question_key = " + CRUDOaRiskProfileQuiz["quiz_question_key"]
+		}
+
+		_, err = tx.Exec(qCRUDOaRiskProfileQuiz)
+		if err != nil {
+			tx.Rollback()
+			log.Println(err.Error())
+			return err, OaRequestKey
+		}
+	}
+
+	// GET RISK PROFILE KEY
+	var RiskProfileKey int64
+	qRiskProfileKey := `SELECT risk_profile_key FROM ms_risk_profile WHERE rec_status = 1 
+	AND ` + strconv.FormatInt(total_score, 10) + ` >= min_score 
+	AND ` + strconv.FormatInt(total_score, 10) + ` <= max_score 
+	ORDER BY risk_profile_key ASC LIMIT 1`
+	err = db.Db.Get(&RiskProfileKey, qRiskProfileKey)
+	if err != nil {
+		tx.Rollback()
+		log.Println(err.Error())
+		return err, OaRequestKey
+	}
+
+	CRUDOaRiskProfile := make(map[string]string)
+	CRUDOaRiskProfile["rec_status"] = paramsOaRequest["rec_status"]
+	CRUDOaRiskProfile["rec_created_by"] = paramsOaRequest["rec_created_by"]
+	CRUDOaRiskProfile["rec_created_date"] = paramsOaRequest["rec_created_date"]
+	CRUDOaRiskProfile["oa_request_key"] = strconv.FormatInt(OaRequestKey, 10)
+	CRUDOaRiskProfile["score_result"] = strconv.FormatInt(total_score, 10)
+	CRUDOaRiskProfile["risk_profile_key"] = strconv.FormatInt(RiskProfileKey, 10)
+
+	qCekRows := `SELECT count(oa_risk_profile_key) FROM oa_risk_profile WHERE rec_status = 1 AND oa_request_key = ` + strconv.FormatInt(OaRequestKey, 10)
+	var countRows uint64
+	err = db.Db.Get(&countRows, qCekRows)
+	if err != nil {
+		tx.Rollback()
+		log.Println(err.Error())
+		return err, OaRequestKey
+	}
+
+	qCRUDOaRiskProfile := ``
+
+	if countRows == 0 { // JIKA TIDAK ADA MAKA CREATE
+		qCRUDOaRiskProfile = GenerateInsertQuery("oa_risk_profile", CRUDOaRiskProfile)
+	} else {
+		qCRUDOaRiskProfile = GenerateUpdateQuery("oa_risk_profile", "oa_request_key", CRUDOaRiskProfile)
+	}
+
+	_, err = tx.Exec(qCRUDOaRiskProfile)
+	if err != nil {
+		tx.Rollback()
+		log.Println(err.Error())
+		return err, OaRequestKey
+	}
+
+	tx.Commit()
+	return nil, OaRequestKey
 }
 
 func GetCountryList(paramSearch map[string]string) []CountryModels {
@@ -499,4 +600,33 @@ func GetCityList(paramSearch map[string]string) []CityModels {
 	}
 
 	return result
+}
+
+type OaQuizAnswer struct {
+	QuestionKey int64 `db:"quiz_question_key" json:"quiz_question_key"`
+	OptionKey   int64 `db:"quiz_option_key" json:"quiz_option_key"`
+}
+
+type OaRequestBankAccountDetails struct {
+	BankAccountKey  *uint64 `db:"bank_account_key" json:"bank_account_key"`
+	BankKey         *uint64 `db:"bank_key" json:"bank_key"`
+	BankAccountNo   *string `db:"account_no" json:"account_no"`
+	BankAccountName *string `db:"account_holder_name" json:"account_holder_name"`
+	BankBranchName  *string `db:"branch_name" json:"branch_name"`
+	CurrencyKey     *uint64 `db:"currency_key" json:"currency_key"`
+	FlagPriority    *uint64 `db:"flag_priority" json:"flag_priority"`
+}
+
+type CountryModels struct {
+	CountryKey  uint64  `db:"country_key" json:"country_key"`
+	CountryCode string  `db:"country_code" json:"country_code"`
+	CountryName string  `db:"country_name" json:"country_name"`
+	CallingCode *string `db:"calling_code" json:"calling_code"`
+}
+
+type CityModels struct {
+	CityKey    uint64  `db:"city_key" json:"city_key"`
+	CityCode   string  `db:"city_code" json:"city_code"`
+	CityName   string  `db:"city_name" json:"city_name"`
+	PostalCode *string `db:"postal_code" json:"postal_code"`
 }
